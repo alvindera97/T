a43ea0368dc03c949680d697973238d0a0da1dea
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-06-16 18:36:37 +0100

    Add Module For Testing [user/client] Roles

---
+++ tests/roles/test_roles.py
@@ -0,0 +1,24 @@
+"""
+Module for tests on [user] roles.
+
+This module contains test cases for testing user roles and their functionality.
+
+Classes:
+  TestRolesTestCase: Provides test methods for unit tests on user roles.
+
+Methods:
+  setUp(): Prepares test environment before each individual test.
+  tearDown(): Cleans up test environment after each test.
+"""
+import unittest
+
+
+class TestRolesTestCase(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def teatDown(self):
+        super().tearDown()
+
+if __name__ == "__main__":
+    unittest.main()
