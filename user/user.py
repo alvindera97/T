543c85a4154c5d8c6bfc906741def4b39792577e
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-06-18 16:03:30 +0100

    🆕: Add 'user' Module And User Class
    Required for defining User entity

---
+++ user/user.py
@@ -0,0 +1,17 @@
+"""
+User module (also referable to as 'Client' module)
+
+This module contains the class and module definitions for the User (Client) entity.
+
+Classes:
+  User: Provides definitions for the User entity object and its functionalities.
+
+"""
+
+
+class User:
+    """
+    Class definition for the User entity
+    """
+
+    pass
