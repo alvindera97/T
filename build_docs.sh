6be73516ae7f18a027e8d340e28f206990bb7c86
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-07-01 12:27:11 +0100

    📖♻️: Update Docs Build Script And pre-commit Hook To Cover Entire Project

--- build_docs.sh
+++ build_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the Sphinx documentation directory
-cd docs
+cd docs || exit
 
 # Build the docs .rst files
 sphinx-apidoc -o source/ ../
@@ -11,3 +11,11 @@ make html
 
 # Navigate back to the root directory
 cd .. || exit
+
+# Check if there are any unstaged changes in the docs build directory
+if [[ -n $(git status -s docs/build/html) ]]; then
+    echo "Documentation needs rebuilding. Please stage the changes and commit again."
+    exit 1
+fi
+
+exit 0
