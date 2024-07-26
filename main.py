86721e5a895150f5d87cfee729ce74160660ed2a
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-07-26 19:12:12 +0100

    🧹: Reformat File
    Reformat main

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ def main(system_argument: List[Optional[str]]):
         print("Enter comma separated list of telegram phone numbers:")
         PHONE_NUMBERS: List[str] = extract_phone_numbers(input())
         if not PHONE_NUMBERS:
-            print("Invalid phone numbers. All phone numbers must be comma separated and each must include country code (+)")
+            print(
+                "Invalid phone numbers. All phone numbers must be comma separated and each must include country code (+)")
         else:
             print("Enter group chat context (mandatory):")
             group_chat_context = input().strip()
