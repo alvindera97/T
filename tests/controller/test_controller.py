aeab6a62afa753be18e051acfe5a8704c8d50183
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-08-11 02:51:54 +0100

    Add New Tests For Controller Constructor Kwargs

---
+++ tests/controller/test_controller.py
@@ -0,0 +1,28 @@
+"""
+Module for tests on application controller
+
+This module contains test cases or testing the "Controller" (otherwise called Application Controller) entity
+and all related functionality.
+
+Classes:
+  ApplicationControllerTestCase
+"""
+import random
+import unittest
+
+
+class ApplicationControllerTestCase(unittest.TestCase):
+    """
+    Test case class for Application Controller
+    """
+
+    def test_controller_constructor_has_array_of_unique_users_based_on_constructor_argument(self) -> None:
+        """
+        Test that the controller constructor takes in kwarg for number of users and populates participating_users
+        attribute with number of unique User objects is specified by the keyword argument.
+        :return: None
+        """
+        number_of_participating_users = random.randint(2, 10)
+        controller = Controller(number_of_participating_users)
+
+        self.assertEqual(len(set(controller.participating_users)), number_of_participating_users)
