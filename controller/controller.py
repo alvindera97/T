a65ed2191fb28a06c54ddae6b85c18148938ca81
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-08-11 03:26:46 +0100

    Flesh Out Constructor Of Controller

--- controller/controller.py
+++ controller/controller.py
@@ -10,3 +10,14 @@ class Controller:
     """
     Class definition for application controller.
     """
+
+    def __init__(self, number_of_users: int) -> None:
+        """
+        Constructor for Controller object.
+
+        The controller performs lightly as a Controller from the MVC Design Pattern
+        https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
+        :param number_of_users:  Number of users to participate in application lifecycle
+        """
+
+        self.participating_users = [User() for _ in range(number_of_users)]
