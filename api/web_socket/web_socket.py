31b8722a29a168a19734f705cfd33891fdc29276
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-08-18 01:19:50 +0100

    Fix Invalid Type Comparison At Chat Websocket
    Fix direct equality comparison of UUID object to non-UUID object

--- api/web_socket/web_socket.py
+++ api/web_socket/web_socket.py
@@ -21,5 +21,5 @@ async def handle_chat(websocket: WebSocket, chat_uuid: uuid.UUID):
     """
     await websocket.accept()
 
-    if chat_uuid != os.getenv('TEST_CHAT_UUID'):
+    if chat_uuid.__str__() != os.getenv('TEST_CHAT_UUID'):
         raise Exception("Invalid chat URL")
