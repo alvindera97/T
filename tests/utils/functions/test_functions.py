76b45620dd59ff11996f09ddfb106628773218ea
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-08-25 01:43:07 +0100

    Add Utility Function To Generate Message From User.generate_message

--- tests/utils/functions/test_functions.py
+++ tests/utils/functions/test_functions.py
@@ -7,12 +7,16 @@ function module.
 
 Classes:
   TestCreateMessageJSON
+  TestGenerateMessage
 """
 
 import unittest
+from unittest.mock import patch
 
 from pydantic import ValidationError
 
+from message import MessageJSON
+from user import User
 from utils.functions import utility_functions as utils
 
 
@@ -34,3 +38,69 @@ class TestCreateMessageJSON(unittest.TestCase):
             utils.create_message_JSON("some message string", 0, 0, 0, 0)
         except ValidationError:
             self.fail("Function doesn't return result with expected schema.")
+
+
+class TestGenerateMessage(unittest.TestCase):
+    """
+    Test case class for tests on function for generating message.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User("some name", "some url")
+
+    def test_function_takes_User_object_argument_where_the_passed_object_must_already_be_instantiated(self) -> None:
+        """
+        Test that the function takes User object argument which must have already been instantiated.
+        :return: None
+        """
+
+        self.assertRaises(AssertionError, lambda: utils.generate_message_from_user(User, 0, 0, 0, 0))
+        try:
+            utils.generate_message_from_user(self.user, 0, 0, 0, 0)
+        except Exception:
+            self.fail("Exception raised while generating message from User via utils.")
+
+    def test_function_takes_other_other_arguments_including_instantiated_user_object_to_populate_return_value(
+            self) -> None:
+        """
+        Test that function takes arguments as well as instantiated User object to populate return value.
+        :return: None
+        """
+        self.assertRaises(AssertionError, lambda: utils.generate_message_from_user(User, 0, 0, 0, 0))
+
+        try:
+            utils.generate_message_from_user(self.user, 0, 0, 0, 0)
+        except Exception:
+            self.fail("Exception raised while generating message from User via utils.")
+
+    @patch("user.User.generate_message", return_value="hello world")
+    def test_function_calls_User_generate_message_method_once_during_function_call(self,
+                                                                                   user_generate_message_mock) -> None:
+        """
+        Test that User's generate_message method was called during execution of function as it is the function that
+        generates the message content for the user.
+        :return: None
+        """
+        utils.generate_message_from_user(self.user, 0, 0, 0, 0)
+
+        user_generate_message_mock.assert_called_once_with("")
+
+    @patch("user.User.generate_message", return_value="hello world")
+    def test_function_return_type_is_of_expected_json_type(self, *_) -> None:
+        """
+        Test that the function returns a result of expected type.
+        :return: None
+        """
+        self.assertTrue(isinstance(utils.generate_message_from_user(self.user, 0, 0, 0, 0), MessageJSON))
+
+    @patch("user.User.generate_message", return_value="hello world")
+    def test_function_returns_message_json_with_different_context_id_from_passed_context_id_if_parent_id_is_None(
+            self, *_) -> None:
+        """
+        Test that function returns message json with context_id different to that of passed context_id if parent_id is None.
+        :return: None
+        """
+        context_id = 0
+        message_json = utils.generate_message_from_user(self.user, 0, 0, 0)
+        self.assertNotEqual(message_json.context_id, context_id)
--- utils/functions/utility_functions.py
+++ utils/functions/utility_functions.py
@@ -3,7 +3,12 @@ Utility functions' module.
 
 This module contains utility functions used in other modules.
 """
+import asyncio
+import random
+from typing import Optional
+
 from message import MessageJSON
+from user import User
 
 
 def create_message_JSON(content: str, thread_id: int, context_id: int, partition_hint: int,
@@ -26,3 +31,36 @@ def create_message_JSON(content: str, thread_id: int, context_id: int, partition
             "partition_hint": partition_hint,
             "parent_message_id": parent_message_id
         })
+
+
+def generate_message_from_user(
+        user: User,
+        thread_id: int,
+        context_id: int,
+        partition_hint: int,
+        parent_message_id: Optional[int] = None,
+) -> MessageJSON:
+    """
+    Given user object, generate and return message from user.
+
+    :param user: Instantiated user object
+    :param thread_id: Thread ID
+    :param context_id: Context ID
+    :param partition_hint: Partition Hint
+    :param parent_message_id: Parent Message ID (optional)
+    :return: MessageJSON of the generated message.
+    """
+    assert isinstance(user, User)
+
+    return create_message_JSON(
+        thread_id=thread_id,
+        context_id=context_id if parent_message_id else random.choice(
+            list(
+                {i for i in range(0, 1000)}.  # TODO: Refactor new context_id generation
+                difference({context_id})
+            )
+        ),
+        partition_hint=partition_hint,
+        parent_message_id=parent_message_id,
+        content=asyncio.run(user.generate_message(""))
+    )
