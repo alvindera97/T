99688e4a52bd78b282c956f01f099ff9e51610a4
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-10-25 15:11:33 +0100

    Replace Default NextJS <Page /> With <Home />
    Replace default NextJS <Page /> component with <Home /> component with lazy loading accounted for in tests.

--- view/web/__test__/page.test.tsx
+++ view/web/__test__/page.test.tsx
@@ -1,51 +1,60 @@
 import { expect, describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "../src/app/page";
 
 describe("Home Page Initial Load", async () => {
   const { container } = render(<Home />);
 
-  it("Assert home page renders new chat form.", () => {
-    const newChatForm = container.querySelector("form");
-    const newChatFormGroupInputs = newChatForm?.querySelectorAll("input");
-    const newChatFormStartChatButton = newChatForm?.querySelectorAll("button");
-    const newChatFormInputDetails = Array.from(
-      newChatFormGroupInputs !== undefined && newChatFormGroupInputs.length > 0
-        ? newChatFormGroupInputs!.values()
-        : []
-    ).map((v) => v);
+  await waitFor(() =>
+    expect(screen.getByText("Start chat")).toBeInTheDocument()
+  );
+  expect(screen.getByText("Start chat")).toBeInTheDocument();
 
-    expect(newChatForm).toBeDefined();
-    expect(newChatFormGroupInputs?.length).toEqual(2);
-    expect(
-      newChatFormInputDetails.find(
-        (e) =>
-          e.id === "new-group-chat-name" &&
-          e.type === "text" &&
-          e.placeholder == "Group Chat name"
-      )
-    );
-    expect(
-      newChatFormInputDetails.find(
-        (e) =>
-          e.id === "new-group-chat-number-of-users" &&
-          e.type === "number" &&
-          e.placeholder == "Number of group chat users"
-      )
-    );
-    expect(
-      newChatFormInputDetails.find(
-        (e) =>
-          e.id === "new-group-chat-context" &&
-          e.type === "text" &&
-          e.placeholder ==
-            "Chat Context - General topic, mood and sentiment of the group chat."
-      )
-    );
-    expect(newChatFormStartChatButton?.length).toEqual(1);
-    expect(newChatFormStartChatButton?.item(0).id).toEqual(
-      "start-group-chat-btn"
-    );
-    expect(newChatFormStartChatButton?.item(0).type).toEqual("submit");
+  it("Assert home page renders new chat form.", () => {
+    waitFor(() => {
+      const newChatForm = container.querySelector("form");
+      const newChatFormGroupInputs = newChatForm?.querySelectorAll("input");
+      const newChatFormStartChatButton =
+        newChatForm?.querySelectorAll("button");
+      const newChatFormInputDetails = Array.from(
+        newChatFormGroupInputs !== undefined &&
+          newChatFormGroupInputs.length > 0
+          ? newChatFormGroupInputs!.values()
+          : []
+      ).map((v) => v);
+
+      expect(newChatForm).toBeDefined();
+      expect(newChatFormGroupInputs?.length).toEqual(2);
+      expect(
+        newChatFormInputDetails.find(
+          (e) =>
+            e.id === "new-group-chat-name" &&
+            e.type === "text" &&
+            e.placeholder == "Group Chat name"
+        )
+      );
+      expect(
+        newChatFormInputDetails.find(
+          (e) =>
+            e.id === "new-group-chat-number-of-users" &&
+            e.type === "number" &&
+            e.placeholder == "Number of group chat users"
+        )
+      );
+      expect(
+        newChatFormInputDetails.find(
+          (e) =>
+            e.id === "new-group-chat-context" &&
+            e.type === "text" &&
+            e.placeholder ==
+              "Chat Context - General topic, mood and sentiment of the group chat."
+        )
+      );
+      expect(newChatFormStartChatButton?.length).toEqual(1);
+      expect(newChatFormStartChatButton?.item(0).id).toEqual(
+        "start-group-chat-btn"
+      );
+      expect(newChatFormStartChatButton?.item(0).type).toEqual("submit");
+    });
   });
 });
