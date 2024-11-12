1a2275b60c4ba63dc154915a69cf05246e0949e5
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-11-13 00:52:34 +0100

    Set Up Vitest To Use Environment Variables

--- view/web/vitest.config.ts
+++ view/web/vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["setupTests.ts"],
+    env: loadEnv("", process.cwd(), ""),
   },
 });
