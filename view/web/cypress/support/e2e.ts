57e23e3a79706fc0af84e5b4251972e92ef518fc
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-12-08 20:41:19 +0100

    Handle Unavailability Of cypro.randomUUID() On Cypress Firefox
    Patch for some versions of firefox where crypto.randomUUID() is not available.

--- view/web/cypress/support/e2e.ts
+++ view/web/cypress/support/e2e.ts
@@ -18,6 +18,19 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+if (!crypto.randomUUID) {
+  crypto.randomUUID = () => {
+    return ([1e7].toString() + "-1e3-4e3-8e3-1e11").replace(/[018]/g, (c) => {
+      const random = crypto.getRandomValues(new Uint8Array(1))[0];
+      return (
+        parseInt(c, 16) ^
+        (random & (15 >> (parseInt(c, 16) / 4)))
+      ).toString(16);
+    }) as `${string}-${string}-${string}-${string}-${string}`;
+  };
+}
+
 let i = 0;
 const throttleTimes = [10, 3200, 10, 10, 10]; // This isn't sustainable
 export const chatUUID = crypto.randomUUID();
