82b043d3883c4d8c002ec5afb79067d918d4d3cb
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2024-12-29 17:50:04 +0100

    Add chat_number_of_users Field To Chat Model

--- models/chat.py
+++ models/chat.py
@@ -6,7 +6,7 @@ This module contains class and module definitions for the Message model
 
 import uuid
 
-from sqlalchemy import Column, UUID, String
+from sqlalchemy import Column, UUID, String, Integer
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -22,3 +22,6 @@ class Chat(Base):
     chat_title = Column("chat_title", String, nullable=False)
     uuid = Column("uuid", UUID, primary_key=True, default=uuid.uuid4)
     chat_context = Column("chat_context", String, nullable=False)
+    chat_number_of_users = Column(
+        "chat_number_of_users", Integer, nullable=False, default=1
+    )
