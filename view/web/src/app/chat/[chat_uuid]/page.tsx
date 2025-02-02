81b0c9e5ac3ab848fdd24e9ac3151df2075ac5ab
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2025-02-02 16:08:43 +0100

    Replace Client Rendering At Chat Page
    Replace with <ChatTitle /> component thereby leaving the chat page component as server component.

--- view/web/src/app/chat/[chat_uuid]/page.tsx
+++ view/web/src/app/chat/[chat_uuid]/page.tsx
@@ -1,26 +1,13 @@
-"use client";
+import { ChatTitle } from "../../../components/ui/ChatTitle";
 
-import React, { useEffect, useState } from "react";
-
-export default function Chat({ params }: { params: { chat_uuid: string } }) {
-  const [chatUuid, setChatUuid] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (params.chat_uuid) {
-      setChatUuid(params.chat_uuid); // Directly access the chat_uuid
-    }
-  }, [params]);
-
-  if (!chatUuid) {
-    return <div>Loading...</div>;
-  }
-
+export default async function Chat({
+  params,
+}: {
+  params: { chat_uuid: string };
+}) {
   return (
     <div className={"pl-8 pt-8"}>
-      <h1 className={"text-7xl"}>Hello world</h1>
-      <p>
-        Chat UUID: <span className={"text-semibold text-3xl"}>{chatUuid}</span>
-      </p>
+      <ChatTitle params={params} />
     </div>
   );
 }
